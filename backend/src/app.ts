7195eeda9fd50a0f518a27034625a84fd7f091e1
Sandaru <87793405+IT21001352@users.noreply.github.com>
2023-10-14 13:55:09 +0530

    heroku port fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import connect from "./utils/connect";
 import logger from "./utils/logger";
 import createServer from "./utils/server";
 
-const port = config.get<number>("port");
+const port = process.env.PORT || config.get<number>("port");
 
 const app = createServer();
 
