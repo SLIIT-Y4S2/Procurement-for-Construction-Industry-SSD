9f091bc42273a07e9dcf0de37c926342ef83f3fd
Sandaru <87793405+IT21001352@users.noreply.github.com>
2023-10-29 07:23:06 +0530

    home screen conflict

--- flutter_client/lib/screens/home_screen.dart
+++ flutter_client/lib/screens/home_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_client/blocs/auth/auth_bloc.dart';
 import 'package:flutter_client/screens/create_requisition_order_page.dart';
 import 'package:flutter_client/constants.dart';
-import 'package:flutter_client/screens/my_orders.dart';
 import 'package:flutter_client/screens/my_orders_screen.dart';
 import 'package:flutter_client/screens/delivery_advice.dart';
 
@@ -18,8 +17,7 @@ class HomeScreen extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.account_circle),
-            onPressed: () {
-            },
+            onPressed: () {},
           ),
         ],
         leading: IconButton(
@@ -48,13 +46,11 @@ class HomeScreen extends StatelessWidget {
                 elevation: 8,
                 color: kSeedColor,
                 child: Container(
-                  width: double
-                      .infinity, 
+                  width: double.infinity,
                   child: Padding(
                     padding: const EdgeInsets.all(16.0),
                     child: Column(
-                      crossAxisAlignment:
-                          CrossAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Icon(
                           Icons.create_new_folder,
@@ -93,7 +89,7 @@ class HomeScreen extends StatelessWidget {
                   Navigator.of(context).push(
                     MaterialPageRoute(
                       builder: (context) {
-                        return MyOrders();
+                        return const MyOrders();
                       },
                     ),
                   );
@@ -153,13 +149,11 @@ class HomeScreen extends StatelessWidget {
                 elevation: 8,
                 color: kSeedColor,
                 child: Container(
-                  width: double
-                      .infinity, 
+                  width: double.infinity,
                   child: Padding(
                     padding: const EdgeInsets.all(16.0),
                     child: Column(
-                      crossAxisAlignment:
-                          CrossAxisAlignment.start, 
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Icon(
                           Icons.edit,
@@ -195,6 +189,12 @@ class HomeScreen extends StatelessWidget {
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
 //         appBar: AppBar(
 //           title: const Text('Home'),
 //           actions: <Widget>[
@@ -217,195 +217,194 @@ class HomeScreen extends StatelessWidget {
 //             },
 //           ),
 //         ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Hi, Supun",
-                  style: TextStyle(fontSize: 24),
-                ),
-                const SizedBox(height: 15),
-                InkWell(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const CreateRequisitionOrder(),
-                      ),
-                    );
-                  },
-                  //Create Order Card
-                  child: Card(
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.zero),
-                    ),
-                    elevation: 8,
-                    color: Theme.of(context).colorScheme.primary,
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Icon(
-                              Icons.create_new_folder,
-                              size: 24,
-                              color: Colors.white,
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              'Create order',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium!
-                                  .copyWith(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              'You can create orders from this.',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium!
-                                  .copyWith(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return const MyOrders();
-                        },
-                      ),
-                    );
-                  },
-                  //My Orders Card
-                  child: Card(
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.zero),
-                    ),
-                    elevation: 8,
-                    color: Theme.of(context).colorScheme.primary,
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Icon(
-                              Icons.view_agenda,
-                              size: 24,
-                              color: Color.fromARGB(255, 255, 255, 255),
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              'My orders',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium!
-                                  .copyWith(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              'You can access all the orders you have taken.',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium!
-                                  .copyWith(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                InkWell(
+        // body: Center(
+        //   child: Padding(
+        //     padding: const EdgeInsets.all(25.0),
+        //     child: Column(
+        //       mainAxisAlignment: MainAxisAlignment.start,
+        //       crossAxisAlignment: CrossAxisAlignment.start,
+        //       children: [
+        //         const Text(
+        //           "Hi, Supun",
+        //           style: TextStyle(fontSize: 24),
+        //         ),
+        //         const SizedBox(height: 15),
+        //         InkWell(
+        //           onTap: () {
+        //             Navigator.of(context).push(
+        //               MaterialPageRoute(
+        //                 builder: (context) => const CreateRequisitionOrder(),
+        //               ),
+        //             );
+        //           },
+        //           //Create Order Card
+        //           child: Card(
+        //             shape: const RoundedRectangleBorder(
+        //               borderRadius: BorderRadius.all(Radius.zero),
+        //             ),
+        //             elevation: 8,
+        //             color: Theme.of(context).colorScheme.primary,
+        //             child: SizedBox(
+        //               width: double.infinity,
+        //               child: Padding(
+        //                 padding: const EdgeInsets.all(16.0),
+        //                 child: Column(
+        //                   crossAxisAlignment: CrossAxisAlignment.start,
+        //                   children: [
+        //                     const Icon(
+        //                       Icons.create_new_folder,
+        //                       size: 24,
+        //                       color: Colors.white,
+        //                     ),
+        //                     const SizedBox(height: 10),
+        //                     Text(
+        //                       'Create order',
+        //                       style: Theme.of(context)
+        //                           .textTheme
+        //                           .bodyMedium!
+        //                           .copyWith(
+        //                               fontSize: 16,
+        //                               fontWeight: FontWeight.bold,
+        //                               color: Colors.white),
+        //                     ),
+        //                     const SizedBox(height: 10),
+        //                     Text(
+        //                       'You can create orders from this.',
+        //                       style: Theme.of(context)
+        //                           .textTheme
+        //                           .bodyMedium!
+        //                           .copyWith(
+        //                               fontSize: 12,
+        //                               fontWeight: FontWeight.bold,
+        //                               color: Colors.white),
+        //                     ),
+        //                   ],
+        //                 ),
+        //               ),
+        //             ),
+        //           ),
+        //         ),
+        //         InkWell(
+        //           onTap: () {
+        //             Navigator.of(context).push(
+        //               MaterialPageRoute(
+        //                 builder: (context) {
+        //                   return const MyOrders();
+        //                 },
+        //               ),
+        //             );
+        //           },
+        //           //My Orders Card
+        //           child: Card(
+        //             shape: const RoundedRectangleBorder(
+        //               borderRadius: BorderRadius.all(Radius.zero),
+        //             ),
+        //             elevation: 8,
+        //             color: Theme.of(context).colorScheme.primary,
+        //             child: SizedBox(
+        //               width: double.infinity,
+        //               child: Padding(
+        //                 padding: const EdgeInsets.all(16.0),
+        //                 child: Column(
+        //                   crossAxisAlignment: CrossAxisAlignment.start,
+        //                   children: [
+        //                     const Icon(
+        //                       Icons.view_agenda,
+        //                       size: 24,
+        //                       color: Color.fromARGB(255, 255, 255, 255),
+        //                     ),
+        //                     const SizedBox(height: 10),
+        //                     Text(
+        //                       'My orders',
+        //                       style: Theme.of(context)
+        //                           .textTheme
+        //                           .bodyMedium!
+        //                           .copyWith(
+        //                             fontSize: 16,
+        //                             fontWeight: FontWeight.bold,
+        //                             color: Colors.white,
+        //                           ),
+        //                     ),
+        //                     const SizedBox(height: 10),
+        //                     Text(
+        //                       'You can access all the orders you have taken.',
+        //                       style: Theme.of(context)
+        //                           .textTheme
+        //                           .bodyMedium!
+        //                           .copyWith(
+        //                             fontSize: 12,
+        //                             fontWeight: FontWeight.bold,
+        //                             color: Colors.white,
+        //                           ),
+        //                     ),
+        //                   ],
+        //                 ),
+        //               ),
+        //             ),
+        //           ),
+        //         ),
+        //         InkWell(
 
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return const Deliveryadvice();
-                        },
-                      ),
-                    );
-                  },
-                  //Dummy Card
-                  child: Card(
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.zero),
-                    ),
-                    elevation: 8,
-                    color: Theme.of(context).colorScheme.primary,
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Icon(
-                              Icons.delivery_dining,
-                              size: 24,
-                              color: Color.fromARGB(255, 255, 255, 255),
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              'Delivery advice',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium!
-                                  .copyWith(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              'You can indicate whether the delivery advice is completed or not.',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium!
-                                  .copyWith(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ));
-  }
-}
+        //           onTap: () {
+        //             Navigator.of(context).push(
+        //               MaterialPageRoute(
+        //                 builder: (context) {
+        //                   return const Deliveryadvice();
+        //                 },
+        //               ),
+        //             );
+        //           },
+        //           //Dummy Card
+        //           child: Card(
+        //             shape: const RoundedRectangleBorder(
+        //               borderRadius: BorderRadius.all(Radius.zero),
+        //             ),
+        //             elevation: 8,
+        //             color: Theme.of(context).colorScheme.primary,
+        //             child: SizedBox(
+        //               width: double.infinity,
+        //               child: Padding(
+        //                 padding: const EdgeInsets.all(16.0),
+        //                 child: Column(
+        //                   crossAxisAlignment: CrossAxisAlignment.start,
+        //                   children: [
+        //                     const Icon(
+        //                       Icons.delivery_dining,
+        //                       size: 24,
+        //                       color: Color.fromARGB(255, 255, 255, 255),
+        //                     ),
+        //                     const SizedBox(height: 10),
+        //                     Text(
+        //                       'Delivery advice',
+        //                       style: Theme.of(context)
+        //                           .textTheme
+        //                           .bodyMedium!
+        //                           .copyWith(
+        //                               fontSize: 16,
+        //                               fontWeight: FontWeight.bold,
+        //                               color: Colors.white),
+        //                     ),
+        //                     const SizedBox(height: 10),
+        //                     Text(
+        //                       'You can indicate whether the delivery advice is completed or not.',
+        //                       style: Theme.of(context)
+        //                           .textTheme
+        //                           .bodyMedium!
+        //                           .copyWith(
+        //                               fontSize: 12,
+        //                               fontWeight: FontWeight.bold,
+        //                               color: Colors.white),
+        //                     ),
+        //                   ],
+        //                 ),
+        //               ),
+        //             ),
+        //           ),
+        //         ),
+        //       ],
+        //     ),
+        //   ),
+        // ));
+
 
 
